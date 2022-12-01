e955abd302ef37867934c200989e64aa515deeca
pigozzif <pigozzife@gmail.com>
2022-12-01 15:32:09 +0100

    revert back and control joints

--- soft_body.py
+++ soft_body.py
@@ -89,8 +89,7 @@ class BaseSoftBody(abc.ABC):
         if name == "tensegrity":
             return TensegritySoftBody(world, pos[0], pos[1])
         elif name == "pressure":
-            return PressureSoftBody(config, world, pos[0], pos[1],
-                                    control_pressure=bool(int(config["control_pressure"])))
+            return PressureSoftBody(config, world, pos[0], pos[1])
         elif name == "voxel":
             return VoxelSoftBody(world, pos[0], pos[1])
         raise ValueError("Invalid soft body name: {}".format(config["body"]))
@@ -108,7 +107,7 @@ class PressureSoftBody(BaseSoftBody):
     mol = 28.0134
     R = 8.31446261815324
 
-    def __init__(self, config, world, start_x, start_y, control_pressure=False):
+    def __init__(self, config, world, start_x, start_y):
         super(PressureSoftBody, self).__init__(world, start_x, start_y)
         self.n_masses = config["n_masses"]
         self.r = config["r"]
@@ -121,7 +120,8 @@ class PressureSoftBody(BaseSoftBody):
         self.joints = []
         self._add_masses(fixture)
         self.sensor = Sensor(self.n_masses * 3 + 2 + 1, 0.25 * 60, self)
-        self.control_pressure = control_pressure
+        self.control_pressure = config["control_pressure"]
+        self.control_joints = config["control_joints"]
         max_p = self.get_maximum_pressure(self.T, self.mass, self.r)
         min_p = max_p * 0.2
         self.pressure = PressureData(self._compute_pressure(), min_p, (max_p - min_p) / 2 + min_p, max_p)
@@ -206,6 +206,8 @@ class PressureSoftBody(BaseSoftBody):
     def apply_control(self, control):
         if self.control_pressure:
             self.pressure.current = min(max(self.pressure.current + control[-1], self.pressure.min), self.pressure.max)
+        if not self.control_joints:
+            return
         for force, joint in zip(control[:-1 if self.control_pressure else 0], self.joints):
             data = joint.userData
             if force >= 0:
